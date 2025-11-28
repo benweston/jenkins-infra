6ac561796dfed890688d61f4a5bb5b25070196c5
Ben Weston <westonb26918@proton.me>
2025-11-28 22:42:40 +0000

    Fix network configuration

--- networks.tf
+++ networks.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_vpc" "jenkins_vpc" {
-  name     = "Jenkins VPC"
-  region   = "lon1"
+  name        = "jenkins-vpc"
+  region      = "lon1"
   description = "Jenkins infra VPC."
-  ip_range = "10.20.0.0/20"
+  ip_range    = "10.20.0.0/20"
 }
